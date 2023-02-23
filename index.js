4dc97bdb859b62afe102507873ea242ba50d78e3
ViniciusOshima <vinicius_hideaki@outlook.com>
2023-02-23 16:55:32 -0300

    Restaurei totalmente a parte de js e estou aplicando nova animação para os botoes da pagina inicial

--- index.js
+++ index.js
@@ -1,38 +1,5 @@
 // GAME HOVER SECTION
 
-let GameHover = document.querySelector('#buttom-game-hover')
-
-let GameButtom = document.querySelector('#buttom-game')
-
-let GameEspaçoFora = document.querySelector('#bg-green')
-
-GameButtom.addEventListener('mouseover', openGamePage)
-
-GameEspaçoFora.addEventListener('mouseover', closeGamePage)
-
-function openGamePage() {
-  GameHover.setAttribute('class', 'answer-buttom')
-  GameHover.innerHTML = `<div>
-  <img src="./images/retro-game.jpg" alt="games" width= "225px">
-</div>
-
-<div class="answer-buttom-info">
-  <h1>JOGUE AGORA</h1>
-<ul>
-  <li class="bg-li-1"><div class="bg-black">Problemas de Lógica</div> </li>
-  <li class="bg-li-2"><div class="bg-black">Dark Stories</div> </li>
-  <li class="bg-li-3"><div class="bg-black">Jogo da Velha</div> </li>
-  <li class="bg-li-4"><div class="bg-black">Adivinhação</div> </li>
-</ul>
-</div>`
-}
-
-function closeGamePage() {
-  GameHover.removeAttribute('class')
-  GameHover.setAttribute('class', 'answer-buttom-none')
-  GameHover.innerHTML = ' '
-}
-
 // ME HOVER SECTION
 
 let MeHover = document.querySelector('#buttom-me')
